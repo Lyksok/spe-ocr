88b38dc9708109367792ff21937acd9df53faa47
Fanette Saury <fanette.saury@compilog.com>
2024-09-14 18:37:36 +0200

    todo buttons

--- source/gtk_app.c
+++ source/gtk_app.c
@@ -8,11 +8,11 @@ activate (GtkApplication* app,
   GtkWidget *label;
 
   window = gtk_application_window_new (app);
-  gtk_window_set_title (GTK_WINDOW (window), "Window");
+  gtk_window_set_title (GTK_WINDOW (window), "OCR App for crosswords");
   gtk_window_set_default_size (GTK_WINDOW (window), 200, 200);
 
   // Create a label widget
-  label = gtk_label_new ("Hello, World!");
+  label = gtk_label_new ("OCR App for crosswords by Fanette Saury, Jans Guillopé and Lise Suzanne");
 
   // Add the label to the window
   gtk_container_add (GTK_CONTAINER (window), label);
@@ -27,7 +27,7 @@ main (int    argc,
   GtkApplication *app;
   int status;
 
-  app = gtk_application_new ("org.gtk.example", G_APPLICATION_FLAGS_NONE);
+  app = gtk_application_new ("org.gtk.OCR", G_APPLICATION_DEFAULT_FLAGS);
   g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
   status = g_application_run (G_APPLICATION (app), argc, argv);
   g_object_unref (app);
