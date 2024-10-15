98a679790c808f221e894873d21d3d924a4b3701
Lily <151917050+Lilytiare@users.noreply.github.com>
2024-10-15 12:57:51 +0200

    update : changed the comments here and there

--- source/solver.c
+++ source/solver.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include "solver.h"
 
+// 1 == TRUE 
+// 0 == FALSE
+
 int Length(char word[]) {
         // Takes a string, a list of characters
         // Returns the length of the word
@@ -22,7 +25,9 @@ int Min(int a, int b) {
 	return a < b ? a : b;
 }
 
-int Search(int row, int col, char mat[row][col], char word[], int r, int c, int len) {
+int Search(int row, int col, char mat[row][col], 
+	char word[], int r, int c, int len) 
+{
 	// we enter here when we found a match for the first letter
 	
 	// will give us the direction in which to search
@@ -70,46 +75,35 @@ int Search(int row, int col, char mat[row][col], char word[], int r, int c, int 
 }
 
 int Solver(int row, int col, char mat[row][col], char word[]) {
-        // Takes a matrice of characters and a word
-	// The function is case sensitive for now :
-	// it does not check for variance in capital and lower letters !
-        // Returns (for now) if the word can be in the matrice or not
-        // --> The only thing checked for now is if the word can be included in terms of size
+	    // Takes a matrice of characters and a word
+		// The function is case sensitive for now :
+		// it does not check for variance in capital and lower letters !
+	    // Returns (for now) if the word can be in the matrice or not
+	    // --> The only thing checked for now is if the word can be included in terms of size
 
         int len = Length(word);
-	int max = Max(row, col);
+		int max = Max(row, col);
         // int max = row > col ? row : col;
         // The maximum number of characters a word can have in this grid
 
         if (len > max) {
-                return 0; // we have an error, convention is to return 1 
-			  // 1 == TRUE
-			  // BUT WE DONT CARE, I'm just doing a true/false
-			  // so let's return 0 (false)
+                return 0; // return FALSE
         }
         else {
-		// it can be in term of size
-		// return 0 is used by convention to end a function normally 
-		// BUT here we don't care about convention as it is a true/false 
-		// 1 == TRUE 
-		// 0 == FALSE 
-
+		// it can be in our matrice in term of size
 
 		int r = 0;	// iterate over rows
 		int c = 0;	// iterate over columns
 
 		char found = 0;	// set to FALSE
-				// used to check if found or not
+						// used to check if found or not
 
 		while (r < row && !found) {
 			while ( c < col && !found) {
 				if (mat[r][c] == word[0]) {
-					// call function ?
-					// have to check the rest of the matrice
+					// we have a match with the first letter of the word
+					// thus we check the rest of the word is in the matrice
 					found = Search(row, col, mat, word, r, c, len);
-					// we want to get TRUE if the function terminated normally
-					// the convention is to return 0 if terminated without problem
-					// but 0 means FALSE, so we !(result) in order to get the actual result
 				}
 				c++;
 			}
