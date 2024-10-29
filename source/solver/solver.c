08469b9c192c8d085e5482c3631a31ec6cff46fd
lise.suzanne <lise.suzanne@epita.fr>
2024-10-29 13:58:48 +0100

    added : a function ToUpper(), and cleaned the rest

--- source/solver/solver.c
+++ source/solver/solver.c
@@ -4,61 +4,114 @@
 // 1 == TRUE 
 // 0 == FALSE
 
-int Length(char word[]) {
-        // Takes a string, a list of characters
-        // Returns the length of the word
-
-        int i = 0;
-        while (word[i] != 0) {
-                i++;
-        }
-        return i;
+/* 
+ * word : a string, list of caracters
+ * Returns the length of the word
+ */
+int Length(char word[])
+{
+	int i = 0;
+	while (word[i] != 0)
+		i++;
+	return i;
 }
 
-int Max(int a, int b) {
-	// returns the max between a and b
+/* 
+ * Returns the max between a and b
+ */
+int Max(int a, int b)
+{
 	return a > b ? a : b;
 }
 
-int Min(int a, int b) {
-	//returns the min between a and b
+/* 
+ * Returns the min between a and b
+ */
+int Min(int a, int b)
+{
 	return a < b ? a : b;
 }
 
+/*
+ * word : a string, assumed filled with only letters
+ * Changes in place the word to upper letters
+ */
+void ToUpper(char word[])
+{
+	size_t i = 0;
+	while (word[i] != 0)
+	{
+		if (word[i] >= 'a')
+			word[i] = word[i] - 32;
+		i++;
+	}
+}
+
+/*
+ * We only enter this function when we found a match for the first letter
+ * mat : a matrice of characters
+ * word : the word to search for
+ * len : the length of the word
+ * Returns True or False if the word is found
+ */
 int Search(int row, int col, char mat[row][col], 
 	char word[], int r, int c, int len) 
 {
-	// we enter here when we found a match for the first letter
-	
-	// will give us the direction in which to search
-	int x[] = { -1, -1, -1, 0, 0, 1, 1, 1 };	// rows
-	int y[] = { -1, 0, 1, -1, 1, -1, 0, 1 };	// columns
-	
+	/*
+	 * will give us the direction in which to search
+	 * x is for the rows
+	 * y is for the columns
+	 * dir is the index over which we will iterate
+	 */
+	int x[] = { -1, -1, -1, 0, 0, 1, 1, 1 };
+	int y[] = { -1, 0, 1, -1, 1, -1, 0, 1 };
 	int dir = 0;
-	int i;		// current index in our word
-	int ri;		// current row in our direction
-	int ci;		// current column in our direction
-	int found = 0;	// set to false at the beginning
-	int noStop;	// use to check if we encounter a stopping case
 
-	while (dir < 8 && !found) {
+	/*
+	 * i : current index in the word
+	 * ri : current row in our direction
+	 * ci : current column in our direction
+	 */
+	int i;
+	int ri;
+	int ci;
+
+	/*
+	 * found : boolean (false at the beginning)
+	 * noStop : boolean checking if we encounter a stopping case
+	 */
+	int found = 0;
+	int noStop;
+
+	while (dir < 8 && !found)
+	{
+		// we update the direction
 		ri = r + x[dir];
 		ci = c + y[dir];
 
-		i = 1;		// we already checked the first character before entering the function
-		noStop = 1;	// set to true here 
+		// we already found the first character
+		i = 1;
+		// set to True
+		noStop = 1;
 		
-		while (i < len && noStop) {
-			if (ri >= row || ri < 0 || ci >= col || ci < 0) {
-				// out of bounds, set to FALSE
+		while (i < len && noStop)
+		{
+			if (ri >= row || ri < 0 || ci >= col || ci < 0)
+			{
+				// out of bounds :
+				// set to FALSE
 				noStop = 0;
 			}
-			else if (mat[ri][ci] != word[i]) {
-				// no match, set to FALSE
+			else if (mat[ri][ci] != word[i]) 
+			{
+				// no match :
+				// set to FALSE
 				noStop = 0;
 			}
-			else {
-				// we have a match, we continue in our directions
+			else
+			{
+				// we have a match :
+				// we continue in our directions
 				i++;
 				ri += x[dir];
 				ci += y[dir];
@@ -74,39 +127,43 @@ int Search(int row, int col, char mat[row][col],
 
 }
 
+/*
+ * mat : a matrice of characters
+ * word : the word to search for
+ * Returns True or False if the word is found
+ */
 int Solver(int row, int col, char mat[row][col], char word[]) {
-	// Takes a matrice of characters and a word
-	// The function is case sensitive for now :
-	// it does not check for variance in capital and lower letters !
-	// Returns (for now) if the word can be in the matrice or not
-	// --> The only thing checked for now is if the word can be included in terms of size
-
+	/*
+	 * len : the length of the word
+	 * max : the max between the numbers of rows and columns
+	 */
         int len = Length(word);
 	int max = Max(row, col);
-        // int max = row > col ? row : col;
-        // The maximum number of characters a word can have in this grid
 
-        if (len > max) {
-                return 0; // return FALSE
-        }
-        else {
-		// it can be in our matrice in term of size
+	// if the word is bigger than what is possible :
+	// return False
+        if (len > max)
+                return 0;
+        else
+	{
+		/*
+		 * r : to iterate over rows
+		 * c : to iterate over columns
+		 * found : boolean, have we found it, set to False
+		 */
+		int r = 0;
+		int c = 0;
+		char found = 0;
 
-		int r = 0;	// iterate over rows
-		int c = 0;	// iterate over columns
-
-		char found = 0;	// set to FALSE
-				// used to check if found or not
-
-		// printf("\nsearching for %s", word);
-		while (r < row && !found) {
+		while (r < row && !found)
+		{
 			c = 0;
-			while ( c < col && !found) {
-				// printf("\nThe current character is '%c'", mat[r][c]);
-				if (mat[r][c] == word[0]) {
-					// printf(" : we have a match !");
-					// we have a match with the first letter of the word
-					// thus we check the rest of the word is in the matrice
+			while ( c < col && !found)
+			{
+				if (mat[r][c] == word[0])
+				{
+					// the first letter of the word match
+					// we check for the rest
 					found = Search(row, col, mat, word, r, c, len);
 				}
 				c++;
--- source/solver/solver.h
+++ source/solver/solver.h
@@ -4,6 +4,7 @@
 int Length(char word[]);
 int Max(int a, int b);
 int Min(int a, int b);
+void ToUpper(char word[]);
 int Search(int row, int col, char mat[row][col], char word[], int r, int c, int len);
 int Solver(int row, int col, char mat[row][col], char word[]);
 
