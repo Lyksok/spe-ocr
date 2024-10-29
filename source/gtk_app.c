029fe30cd82e04ee396357fa51cabffb01d93c38
Fanette Saury <fanette.saury@compilog.com>
2024-10-29 17:22:30 +0100

    fix : sample image before image load

--- source/gtk_app.c
+++ source/gtk_app.c
@@ -1,7 +1,7 @@
 #include <gtk/gtk.h>
 
 // reminder : GTK_WIDGET is a macro that casts the pointer to a GtkWidget, just conventional
-// TODO : refactor load image names ...
+
 
 /**
  * @brief Opens a file chooser dialog to select and load an image file
@@ -9,7 +9,7 @@
  * @param data for the parent window
  * @return loadd GtkImage widgt or NULL if no image was loaded
  */
-GtkImage *load_image_from_file(GtkWidget *widget, gpointer data)
+GtkImage *open_file_dialog(GtkWidget *widget, gpointer data)
 {
   GtkWidget *dialog;
   GtkFileChooserAction action = GTK_FILE_CHOOSER_ACTION_OPEN; // "action" type performed by fc
@@ -17,7 +17,7 @@ GtkImage *load_image_from_file(GtkWidget *widget, gpointer data)
   GtkWidget *image = NULL;
 
   // fc = dialog box used for "File Open" or "File Save" actions
-  dialog = gtk_file_chooser_dialog_new("Open Image",
+  dialog = gtk_file_chooser_dialog_new("Load Image",
                                        GTK_WINDOW(data),
                                        action,
                                        "_Cancel",
@@ -32,15 +32,15 @@ GtkImage *load_image_from_file(GtkWidget *widget, gpointer data)
     char *filename;
     GtkFileChooser *chooser = GTK_FILE_CHOOSER(dialog); // widget for choosing files
     filename = gtk_file_chooser_get_filename(chooser);  // get filename from fc
-    image = gtk_image_new_from_file(filename);  // creates a new image widget from the file
-    g_free(filename); // free filename memory
+    image = gtk_image_new_from_file(filename);          // creates a new image widget from the file
+    free(filename);                                   // free filename memory
   }
 
-  gtk_widget_destroy(dialog);
+  gtk_widget_destroy(dialog); // TODO spotted pb
   return GTK_IMAGE(image);
 }
 
-GtkImage *save_image_from_file(GtkWidget *widget, gpointer data)
+GtkImage *save_file_dialog(GtkWidget *widget, gpointer data)
 {
   GtkWidget *dialog;
   GtkFileChooserAction action = GTK_FILE_CHOOSER_ACTION_SAVE; // "action" type performed by fc
@@ -57,11 +57,12 @@ GtkImage *save_image_from_file(GtkWidget *widget, gpointer data)
  * @param data Additional data for image creation.
  * @return A pointer to the new GtkImage.
  */
-GtkImage *create_image(GtkWidget *widget, gpointer data)
+GtkImage *init_image_widget(GtkWidget *parent, const char *sample_image_path)
 {
-  GtkImage *image = GTK_IMAGE(gtk_image_new());
-  gtk_widget_set_size_request(GTK_WIDGET(image), 450, 450); // Set the size of the placehollder image
-  return image;
+  GtkImage *image = GTK_IMAGE(gtk_image_new_from_file(sample_image_path));
+  // gtk_widget_set_size_request(GTK_WIDGET(image), 450, 450); // Set the size of the placeholder image
+
+    return image;
 }
 
 /**
@@ -69,17 +70,30 @@ GtkImage *create_image(GtkWidget *widget, gpointer data)
  * @param widget The widget that triggered the function
  * @param data Pointer to the image widget to be updated
  */
-void callback_load_image(GtkWidget *widget, gpointer data)
+void load_image_clicked(GtkWidget *widget, gpointer data)
 {
-  GtkWidget *old = gtk_widget_get_parent(GTK_WIDGET(data));
-  GtkImage *new = load_image_from_file(widget, old); // type is GtkImage instead of GtkWidget => bcs of the return type of load_image_from_file
-  // TODO understand 2nd parameter cf docu
-  if (new != NULL)
-  {
-    gtk_widget_destroy(GTK_WIDGET(data));
-    gtk_box_pack_start(GTK_BOX(old), GTK_WIDGET(new), TRUE, TRUE, 0); // TODO understand better relationship parent-child btw widgets
-    gtk_widget_show_all(GTK_WIDGET(new));
-  }
+    if (GTK_IS_WIDGET(data))
+    {
+        GtkWidget *old = gtk_widget_get_parent(GTK_WIDGET(data));
+        if (GTK_IS_WIDGET(old))
+        {
+            GtkImage *new = open_file_dialog(widget, old); // type is GtkImage instead of GtkWidget => bcs of the return type of open_file_dialog
+            if (new != NULL)
+            {
+                gtk_widget_destroy(GTK_WIDGET(data));
+                gtk_box_pack_start(GTK_BOX(old), GTK_WIDGET(new), TRUE, TRUE, 0); // TODO understand better relationship parent-child btw widgets
+                gtk_widget_show_all(GTK_WIDGET(new));
+            }
+        }
+        else
+        {
+            g_warning("Parent widget is not valid.");
+        }
+    }
+    else
+    {
+        g_warning("Data is not a valid widget.");
+    }
 }
 /**
  * @brief Creates a button widget.
@@ -91,10 +105,12 @@ void callback_load_image(GtkWidget *widget, gpointer data)
  * @param data A gpointer to any additional data needed for the callback.
  * @return A pointer to the new GtkButton.
  */
-GtkWidget *create_button(const char *label, GCallback callback, gpointer data)
+GtkWidget *init_button(const char *label, GCallback callback, gpointer data)
 {
   GtkWidget *button = gtk_button_new_with_label(label);
-  g_signal_connect(button, "clicked", callback, data);
+  if (callback != NULL)
+    // connects the "clicked" signal to the callback function
+    g_signal_connect(button, "clicked", callback, data);
   return button;
 }
 /**
@@ -103,7 +119,7 @@ GtkWidget *create_button(const char *label, GCallback callback, gpointer data)
  * GTK window. It adds the menu items to load and save image from a file explorer.
  * @param window A pointer to the GTK window where the menu bar will be added.
  */
-GtkWidget *create_menu_bar(GtkWidget *window)
+GtkWidget *init_menu_bar(GtkWidget *window)
 {
   GtkWidget *menu_bar, *file_menu, *file_menu_item, *load_menu_item, *save_menu_item;
 
@@ -117,36 +133,38 @@ GtkWidget *create_menu_bar(GtkWidget *window)
   gtk_menu_shell_append(GTK_MENU_SHELL(menu_bar), file_menu_item);
 
   // Creates load menu item => file_menu
-  load_menu_item = gtk_menu_item_new_with_label("Open Image");
-  g_signal_connect(load_menu_item, "activate", G_CALLBACK(callback_load_image), window);
+  load_menu_item = gtk_menu_item_new_with_label("Load Image");
+  g_signal_connect(load_menu_item, "activate", G_CALLBACK(load_image_clicked), window);
   gtk_menu_shell_append(GTK_MENU_SHELL(file_menu), load_menu_item);
 
   // Creates save menu item => file_menu
   save_menu_item = gtk_menu_item_new_with_label("Save Image");
-  g_signal_connect(save_menu_item, "activate", G_CALLBACK(save_image_from_file), window);
+  g_signal_connect(save_menu_item, "activate", G_CALLBACK(save_file_dialog), window);
   gtk_menu_shell_append(GTK_MENU_SHELL(file_menu), save_menu_item);
   // Add menu_bar => window
   return menu_bar;
 }
 /**
- * @brief Activates the GTK application OCR !
+ * @brief Activates the app. Precisely, it lays out the widgets and connects the signals.
  * @param app A pointer to the GtkApplication instance.
  */
 static void activate(GtkApplication *app)
 {
   GtkWidget *window, *image, *button, *menu_bar, *vbox, *hbox, *vbox_buttons, *empty_box;
+  // declare pointers on the objects to be initialised
   // ORDER OF WIDGETS DECLARATION IS DETEMINANT FOR THE ORDER OF DISPLAY !!!
   window = gtk_application_window_new(app);
-  gtk_window_set_title(GTK_WINDOW(window), "OCR App for crosswords by Caroline Deliere, Fanette Saury, Jans Guillopé and Lise Suzanne");
+  gtk_window_set_title(GTK_WINDOW(window), "OCR App for Crosswords");
   gtk_window_set_default_size(GTK_WINDOW(window), 800, 600);
   gtk_window_set_resizable(GTK_WINDOW(window), TRUE);
 
   vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
   gtk_container_add(GTK_CONTAINER(window), vbox);
 
-  image = GTK_WIDGET(create_image(window, NULL));
+  image = GTK_WIDGET(init_image_widget(hbox, "images/abstract_background.jpg"));
 
-  menu_bar = create_menu_bar(window);
+
+  menu_bar = init_menu_bar(window);
   gtk_box_pack_start(GTK_BOX(vbox), menu_bar, FALSE, FALSE, 0);
 
   hbox = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 0);
@@ -163,7 +181,7 @@ static void activate(GtkApplication *app)
 
   // empty_boxes + button = vbox
   gtk_box_pack_start(GTK_BOX(vbox_buttons), empty_box, TRUE, TRUE, 0);
-  button = create_button("Grayscale", NULL, NULL);
+  button = init_button("Grayscale", NULL, NULL);
   gtk_widget_set_size_request(button, 100, 50);
   gtk_box_pack_start(GTK_BOX(vbox_buttons), button, FALSE, FALSE, 0);
   gtk_box_pack_start(GTK_BOX(vbox_buttons), empty_box, TRUE, TRUE, 0);
@@ -182,10 +200,17 @@ int main(int argc, char **argv)
   GtkApplication *app;
   int status;
 
+  /*  Window creation  */
+  // gtk_init() called in gtk_application_new '
   app = gtk_application_new("org.gtk.OCR", G_APPLICATION_DEFAULT_FLAGS); // creates a new gtk app instance
-  g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);  // connects the "activate" signal to the "activate" function
-  status = g_application_run(G_APPLICATION(app), argc, argv); // run app
-  g_object_unref(app);  // decrements reference count of the gtk_app ie freeds the memory bcs it's last instruction
+
+  /*  Signal connections */
+  // "activate" signal connection
+  g_signal_connect(app, "activate", G_CALLBACK(activate), NULL); // connects the "activate" signal to the "activate" function
+
+ /*  Run event loop  */
+  status = g_application_run(G_APPLICATION(app), argc, argv);
+  g_object_unref(app);
 
   return status;
 }
