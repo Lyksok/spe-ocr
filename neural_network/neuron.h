eab0e8945b4c9db3aabf5c3d153da02ae61503c8
Caroline <caroline@Ginkgo.home>
2024-10-29 16:06:08 +0100

    It may work, I do not really know

--- neural_network/neuron.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-typedef struct {
-	double *weights;
-	double bias;
-} Neuron;
-
-void initialize_neuron(Neuron *neuron);
-void backward(Neuron *neuron, double *inputs, double target);
