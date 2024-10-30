bdc634372c50dc706cfc45a0c2486e09748ffef0
Caroline <caroline@Ginkgo.home>
2024-10-30 10:54:23 +0100

    numHiddenNodes defined at 3

--- neural_network/neural.c
+++ neural_network/neural.c
@@ -33,7 +33,7 @@ void shuffle(int *array, size_t n)
 }
 
 #define numInputs 2
-#define numHiddenNodes 2
+#define numHiddenNodes 3
 #define numOutputs 1
 #define numTrainingSets 4
 
@@ -57,10 +57,10 @@ int main(void)
 		{1.0f, 1.0f}
 	};
 	double training_output[numTrainingSets][numOutputs] = {
+		{1.0f},
 		{0.0f},
-		{1.0f},
-		{1.0f},
-		{0.0f}
+		{0.0f},
+		{1.0f}
 	};
 
 	for(int i = 0; i < numInputs; i++)
@@ -104,7 +104,8 @@ int main(void)
 
 				for(int k = 0; k < numInputs; k++)
 				{
-					activation += training_inputs[i][k] * hiddenWeights[k][j];
+					activation += training_inputs[i][k] * 
+						hiddenWeights[k][j];
 				}
 
 				hiddenLayer[j] = sigmoid(activation);
@@ -116,13 +117,15 @@ int main(void)
 				double activation = hiddenLayerBias[j];
 				for(int k = 0; k < numHiddenNodes; k++)
 				{
-					activation += hiddenLayer[k] * outputWeights[k][j];
+					activation += hiddenLayer[k] * 
+						outputWeights[k][j];
 				}
 				outputLayer[j] = sigmoid(activation);
 			}
 
-			 printf("Input: [%g, %g]  Output: %g  Predicted Output: %g \n",
-                                        training_inputs[i][0], training_inputs[i][1],
+			 printf("Input: [%g, %g] Output: %g Predicted Output: %g \n",
+                                        training_inputs[i][0], 
+					training_inputs[i][1],
                                         outputLayer[0], training_output[i][0]);
 
 
@@ -133,8 +136,10 @@ int main(void)
 			double deltaOutput[numOutputs];
 			for(int j = 0; j < numOutputs; j++)
 			{
-				double error = (training_output[i][j] - outputLayer[j]);
-				deltaOutput[j] = error * dSigmoid(outputLayer[j]);
+				double error = (training_output[i][j] - 
+						outputLayer[j]);
+				deltaOutput[j] = error * 
+					dSigmoid(outputLayer[j]);
 			}
 			
 			//Compute change in hidden weights
@@ -144,9 +149,11 @@ int main(void)
 				double error = 0.0f;
 				for(int k = 0; k < numOutputs; k++)
 				{
-					error += deltaOutput[k] * outputWeights[j][k];
+					error += deltaOutput[k] * 
+						outputWeights[j][k];
 				}
-				deltaHidden[j] = error * dSigmoid(hiddenLayer[j]);
+				deltaHidden[j] = error * 
+					dSigmoid(hiddenLayer[j]);
 			}
 			
 
@@ -156,7 +163,8 @@ int main(void)
 				outputLayerBias[j] += deltaOutput[j] * lr;
 				for(int k = 0; k < numHiddenNodes; k++)
 				{
-					outputWeights[k][j] += hiddenLayer[k] * deltaOutput[j] * lr;
+					outputWeights[k][j] += hiddenLayer[k] *
+					       	deltaOutput[j] * lr;
 				}
 			}
 
@@ -165,7 +173,8 @@ int main(void)
 				hiddenLayerBias[j] += deltaHidden[j] * lr;
 				for(int k = 0; k < numInputs; k++)
 				{
-					hiddenWeights[k][j] += hiddenLayer[k] * deltaHidden[j] * lr;
+					hiddenWeights[k][j] += hiddenLayer[k] *
+					       	deltaHidden[j] * lr;
 				}
 			}
 		}
