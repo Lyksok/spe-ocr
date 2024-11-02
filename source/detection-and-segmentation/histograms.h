32371f60baef9d993636157db4061790049908f6
Lykso <jans.guillope@gmail.com>
2024-11-02 14:14:03 +0100

    Fix: update header file

--- source/detection-and-segmentation/histograms.h
+++ source/detection-and-segmentation/histograms.h
@@ -2,8 +2,11 @@
 #define HISTOGRAMS_H
 
 #include <SDL2/SDL.h>
+#include "structures.h"
 
 void get_col_histogram(SDL_Surface* surface, int** histogram);
 void get_row_histogram(SDL_Surface* surface, int** histogram);
+BoundingBox*** group_by_rows(SDL_Surface* surface, BoundingBox** boxes, int len, int* n_len);
+BoundingBox*** group_by_cols(SDL_Surface* surface, BoundingBox** boxes, int len, int* n_len);
 
 #endif /* HISTOGRAMS_H */
