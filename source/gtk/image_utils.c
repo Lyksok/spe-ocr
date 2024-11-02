348ac3c632b5383a0d6cfeb872c865965f9bee1e
Fanette Saury <fanette.saury@compilog.com>
2024-11-02 15:00:42 +0100

    FIX : out of range pixel accessed in resize_pixbuf during interpolation algo. Added guards.

--- source/gtk/image_utils.c
+++ source/gtk/image_utils.c
@@ -137,18 +137,26 @@ GdkPixbuf *resize_pixbuf(GdkPixbuf *pixbuf, int new_width, int new_height)
             int left = (int)floor(oldx);
             int right = left + 1;
 
-            for (int c = 0; c < gdk_pixbuf_get_n_channels(pixbuf); c++)
+            if (top < height && left < width && bottom < height && right < width)
             {
-                double top_left = gdk_pixbuf_get_pixels(pixbuf)[top * gdk_pixbuf_get_rowstride(pixbuf) + left * gdk_pixbuf_get_n_channels(pixbuf) + c];
-                double top_right = gdk_pixbuf_get_pixels(pixbuf)[top * gdk_pixbuf_get_rowstride(pixbuf) + right * gdk_pixbuf_get_n_channels(pixbuf) + c];
-                double bottom_left = gdk_pixbuf_get_pixels(pixbuf)[bottom * gdk_pixbuf_get_rowstride(pixbuf) + left * gdk_pixbuf_get_n_channels(pixbuf) + c];
-                double bottom_right = gdk_pixbuf_get_pixels(pixbuf)[bottom * gdk_pixbuf_get_rowstride(pixbuf) + right * gdk_pixbuf_get_n_channels(pixbuf) + c];
+                for (int c = 0; c < gdk_pixbuf_get_n_channels(pixbuf); c++)
+                {
+                    // Get the pixel values at the four corners
+                    double top_left = gdk_pixbuf_get_pixels(pixbuf)[top * gdk_pixbuf_get_rowstride(pixbuf) + left * gdk_pixbuf_get_n_channels(pixbuf) + c];
+                    double top_right = gdk_pixbuf_get_pixels(pixbuf)[top * gdk_pixbuf_get_rowstride(pixbuf) + right * gdk_pixbuf_get_n_channels(pixbuf) + c];
+                    double bottom_left = gdk_pixbuf_get_pixels(pixbuf)[bottom * gdk_pixbuf_get_rowstride(pixbuf) + left * gdk_pixbuf_get_n_channels(pixbuf) + c];
+                    double bottom_right = gdk_pixbuf_get_pixels(pixbuf)[bottom * gdk_pixbuf_get_rowstride(pixbuf) + right * gdk_pixbuf_get_n_channels(pixbuf) + c];
 
-                double top_interp = top_left + (top_right - top_left) * (oldx - left);
-                double bottom_interp = bottom_left + (bottom_right - bottom_left) * (oldx - left);
-                double value = top_interp + (bottom_interp - top_interp) * (oldy - top);
+                    // Interpolate between the top-left and top-right pixels
+                    double top_interp = top_left + (top_right - top_left) * (oldx - left);
+                    // Interpolate between the bottom-left and bottom-right pixels
+                    double bottom_interp = bottom_left + (bottom_right - bottom_left) * (oldx - left);
+                    // Interpolate between the top and bottom interpolated values
+                    double value = top_interp + (bottom_interp - top_interp) * (oldy - top);
 
-                gdk_pixbuf_get_pixels(new_pixbuf)[y * gdk_pixbuf_get_rowstride(new_pixbuf) + x * gdk_pixbuf_get_n_channels(new_pixbuf) + c] = (guchar)value;
+                    // Set the interpolated value in the new pixbuf
+                    gdk_pixbuf_get_pixels(new_pixbuf)[y * gdk_pixbuf_get_rowstride(new_pixbuf) + x * gdk_pixbuf_get_n_channels(new_pixbuf) + c] = (guchar)value;
+                }
             }
         }
     }
@@ -166,7 +174,7 @@ GdkPixbuf *resize_with_borders(GdkPixbuf *pixbuf)
     int original_height = gdk_pixbuf_get_height(pixbuf);
 
     // Step 1: Calculate the diagonal length
-    int diagonal_length = (int)sqrt(original_width * original_width + original_height * original_height);
+    int diagonal_length = (int)ceil(sqrt(original_width * original_width + original_height * original_height));
 
     // Step 2: Add borders to make the image a square with side length equal to the diagonal length
     GdkPixbuf *pixbuf_1 = create_pixbuf_with_alpha_borders(pixbuf, diagonal_length, diagonal_length);
