fbf848eaf494fe3aa3fe7e21623de25fd70c5bf3
lise.suzanne <lise.suzanne@epita.fr>
2024-11-24 13:31:45 +0100

    modified : Makefile (I love it) and added a main (doing nothing for now), going to check stuff for handling surfaces

--- neural_network/convertion.c
+++ neural_network/convertion.c
@@ -6,6 +6,10 @@
 // RGB WHITE = 255, 255, 255
 // RGB BLACK = 0, 0, 0
 
+/*
+ * path : a path to a png image
+ * returns : a surface from the image
+ * */
 SDL_Surface *toSDL(char *path)
 {
 	SDL_Surface *new = IMG_Load(path);
@@ -16,13 +20,29 @@ SDL_Surface *toSDL(char *path)
 	return new;
 }
 
+/*
+ * surface : a surface of black and white pixels
+ * list : a pointer to a list, to be filled with 0 and 1
+ * len : the length of the list
+ * returns : build in place the list
+ * */
 void SDL_to_list(SDL_Surface *surface, int **list, int len)
 {
-	for (int i = 0; i < len; i++)
+	int i = 0;
+	SDL_LockSurface(surface);
+
+	for (; i < len; i++)
 	{
-		printf("%d ", *list[i]);
+		Uint32 pixel = ((Uint32*)surface->pixels)[i];
+		Uint8 r;
+		Uint8 g;
+		Uint8 b;
+		SDL_GetRGB(pixel, surface->format, &r, &g, &b);
+		// we take a surface with only black and white
+		// if one of the parameter is != 0, then it has to be white
+		*list[i] = r ? 1 : 0;
 	}
-	
+	SDL_UnlockSurface(surface);
 	SDL_FreeSurface(surface);
 	return;
 }
---
+++ neural_network/main.c
@@ -0,0 +1,9 @@
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+
+#include "convertion.h"
+
+int main()
+{
+	return 0;
+}
