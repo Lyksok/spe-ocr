0acc857eda0a33749c5cfbb8ee686c34dd256274
Lykso <jans.guillope@gmail.com>
2024-11-28 12:23:37 +0100

    Create: image processing main file

---
+++ source/image_processing/image_processing.c
@@ -0,0 +1,11 @@
+#include "image_processing.h"
+
+void start_procedure(SDL_Surface* surface)
+{
+    // Grayscale
+
+    // Try sauvola
+    // Check detection
+    // If detecting exit
+    // Else try other methods
+}
