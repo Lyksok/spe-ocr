c5b1ae0ef5a3f706b2b50010c3f4f44266a285bd
Lykso <jans.guillope@gmail.com>
2024-11-30 16:11:16 +0100

    Add: main files for detection

---
+++ source/image_processing/detection/detection.c
@@ -0,0 +1,21 @@
+#include "detection.h"
+
+// Compute all bounding_boxes
+
+/* Detects characters in the grid by filtering boxes
+ * using their height and their width to find non-characters
+ * Returns a new list with characters' boxes
+ */
+struct list* detect_characters(struct list* box_list)
+{
+    // TODO
+}
+
+/* Find dimensions of the grid
+ * If found return 0 and set width and height
+ * Else return 1
+ */
+int detect_grid_dimensions(int* width, int* height)
+{
+    // TODO
+}
