fee6a5aea89c1a3fcf24dbb4e1b0c2628df48c8c
Lykso <jans.guillope@gmail.com>
2024-11-30 14:17:11 +0100

    Fix: function return value

--- source/image_processing/utils/pixel_utils.h
+++ source/image_processing/utils/pixel_utils.h
@@ -10,5 +10,6 @@ Uint8 get_gpixel_from_pixel(SDL_Surface* surface, Uint32 pixel);
 Uint8 get_gpixel_from_array(SDL_Surface* surface, int i);
 Uint32* get_pixels_from_surface(SDL_Surface* surface);
 void set_pixels_on_surface(SDL_Surface* surface, Uint32* pixels);
+int pixel_is_background_from_coord(SDL_Surface* surface, int x, int y);
 
 #endif /* PIXEL_UTILS_H */
