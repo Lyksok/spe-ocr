56b445da65fad246c4c6a2ea52af6c46a38d2612
Lykso <jans.guillope@gmail.com>
2024-12-01 17:02:08 +0100

    Fix: scaling issue

--- source/image_processing/utils/sdl_utils.h
+++ source/image_processing/utils/sdl_utils.h
@@ -3,10 +3,12 @@
 
 #include <SDL2/SDL.h>
 #include <err.h>
+#include <math.h>
 #include "../detection/bounding-box.h"
 #include "../detection/point.h"
 
-void draw_rect(SDL_Renderer *renderer, BoundingBox* box);
+void draw_rect(SDL_Surface* surface, SDL_Renderer *renderer, BoundingBox* box,
+        int width, int height);
 void draw_line(SDL_Renderer *renderer, Point p1, Point p2);
 
 #endif /* SDL_UTILS_H */
