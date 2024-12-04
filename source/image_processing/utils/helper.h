1df504f2c28c1150d91b817df306cf7d651d57da
Lykso <jans.guillope@gmail.com>
2024-12-04 13:05:54 +0100

    Add: sorting algorithm

--- source/image_processing/utils/helper.h
+++ source/image_processing/utils/helper.h
@@ -1,7 +1,15 @@
 #ifndef HELPER_H
 #define HELPER_H
 
+#include <stdlib.h>
+
 int min(int a, int b);
 int max(int a, int b);
+
+// Sort ascending comparison function
+int compare_ascending(const void* a, const void* b);
+
+// Sort array using compar comparison function
+void sort_array(int** arr, int len, int (*compar)(const void *, const void*));
 
 #endif /* HELPER_H */
