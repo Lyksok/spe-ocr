ee35d2df93c1f52ec4f6bed66d1ac5190230b8c8
Fanette Saury <fanette.saury@compilog.com>
2024-12-04 23:51:34 +0100

    Merge edge detection files from main

---
+++ source/image_processing/edge_detection/canny.h
@@ -0,0 +1,18 @@
+#ifndef CANNY_H
+#define CANNY_H
+
+#include <SDL2/SDL.h>
+#include "convolution.h"
+#include "gaussian.h"
+#include "../utils/converting.h"
+#include "../utils/pixel_utils.h"
+#include <math.h>
+#include <stdlib.h>
+
+void canny_edge_detection(SDL_Surface *surface);
+void non_maximum_suppression(SDL_Surface *magnitude, SDL_Surface *direction);
+void double_threshold(SDL_Surface *magnitude);
+static double *get_sobel_mask_x();
+static double *get_sobel_mask_y();
+
+#endif
