8e6769fcc8fd79a6ff9ec72b28405252ce4370a2
Lykso <jans.guillope@gmail.com>
2024-12-04 13:08:59 +0100

    Add: point in width or height

--- source/image_processing/detection/point.h
+++ source/image_processing/detection/point.h
@@ -9,6 +9,12 @@ int point_are_equal(Point p1, Point p2);
 // Check if a point is in a box
 int point_is_in_box(Point point, BoundingBox* box);
 
+// Check if a point x componant is within the box
+int point_is_in_box_width(Point point, BoundingBox* box);
+
+// Check if a point y componant is within the box
+int point_is_in_box_height(Point point, BoundingBox* box);
+
 // Check if a point is in the list of box
 int point_is_in_box_list(Point point, struct list* list);
 
