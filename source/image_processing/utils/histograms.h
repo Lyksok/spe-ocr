86509472f7b1a4112bdd3f732ec39c52d3a95304
Fanette Saury <fanette.saury@compilog.com>
2024-12-06 01:08:43 +0100

    update histogram.h

--- source/image_processing/utils/histograms.h
+++ source/image_processing/utils/histograms.h
@@ -9,5 +9,9 @@ void create_histogram_of_pixel(SDL_Surface *surface, int **histogram,
 void create_histogram_of_surface(SDL_Surface *surface, int **histogram,
                                  void (*func)(int **, size_t, size_t, SDL_Surface *));
 void histogram_of_pixels(SDL_Surface *surface, int **histogram);
+void histogram_of_pixels_with_alpha(int **histogram, Uint32 pixel, SDL_Surface *surface);
+void count_by_pixel(int **histogram, Uint32 pixel, SDL_Surface *surface);
+void get_bounding_box_freq_histograms(SDL_Surface *surface,
+                                      BoundingBox **boxes, int size, int **column_histo, int **row_histo);
 
 #endif /* HISTOGRAMS_H */
