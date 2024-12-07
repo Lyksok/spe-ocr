5c7589cd9e32cd177149b6fbfc2a88c748a2c391
Fanette Saury <fanette.saury@compilog.com>
2024-12-07 02:06:19 +0100

    merge last canny version from fanette/gui_integrations

--- source/image_processing/edge_detection/canny.h
+++ source/image_processing/edge_detection/canny.h
@@ -6,13 +6,14 @@
 #include "gaussian.h"
 #include "../utils/converting.h"
 #include "../utils/pixel_utils.h"
+#include "../parameters.h"
 #include <math.h>
 #include <stdlib.h>
 
-void canny_edge_detection(SDL_Surface *surface);
-void non_maximum_suppression(SDL_Surface *magnitude, SDL_Surface *direction);
-void double_threshold(SDL_Surface *magnitude);
-static double *get_sobel_mask_x();
-static double *get_sobel_mask_y();
+void canny_edge_detection(SDL_Surface *surface, struct parameters *param);
+void non_maximum_suppression(SDL_Surface* surface, Uint8**magnitude, Uint8**direction);
+void double_threshold(SDL_Surface* surface, Uint8**magnitude);
+double *get_sobel_mask_x();
+double *get_sobel_mask_y();
 
 #endif
---
+++ source/image_processing/edge_detection/hough_transform.h
@@ -0,0 +1,6 @@
+#ifndef HOUGH_TRANSFORM_H
+#define HOUGH_TRANSFORM_H
+
+void hough_transform(const unsigned char *edges, int width, int height, int *accumulator, int acc_width, int acc_height);
+
+#endif // HOUGH_TRANSFORM_H
